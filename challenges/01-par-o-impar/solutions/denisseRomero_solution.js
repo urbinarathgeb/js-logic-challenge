46259b7cd344b0fae33931325981cc688dc9753a
Denisse Romero <denisse.romero.c@gmail.com>
2026-01-24 23:38:34 -0600

    Solución del reto 01: determinar si un número es par o impar

---
+++ challenges/01-par-o-impar/solutions/denisseRomero_solution.js
@@ -0,0 +1,16 @@
+/**
+ * NOMBRE DEL RETO: [Par o Impar]
+ * SOLUCIONADO POR: [Denisse Romero]
+ */
+
+// ESCRIBE TU CÓDIGO AQUÍ
+let numero = 4;
+  if (numero % 2 === 0) {
+    console.log("par");
+  } else {
+    console.log("impar");
+  }
+
+
+// Pruebas (esto sirve para verificar)
+
