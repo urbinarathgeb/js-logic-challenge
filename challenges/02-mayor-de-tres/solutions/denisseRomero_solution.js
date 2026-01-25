e1133de3a19bdedcc87a1cad8ba788913c3da003
Denisse Romero <denisse.romero.c@gmail.com>
2026-01-25 01:35:18 -0500

    Solución reto mayor de tres Denisse Romero

---
+++ challenges/02-mayor-de-tres/solutions/denisseRomero_solution.js
@@ -0,0 +1,20 @@
+/**
+ * NOMBRE DEL RETO: [mayor de tres]
+ * SOLUCIONADO POR: [Denisse Romero]
+ */
+let num1 = 50;
+let num2 = 20;
+let num3 = 15;
+
+
+
+if (num1 > num2 && num1 > num3) {
+    console.log(num1);
+  } else if (num2 > num1 && num2 > num3) {
+    console.log(num2);
+  } else {
+    console.log(num3);
+  };
+
+
+// Pruebas (esto sirve para verificar)
