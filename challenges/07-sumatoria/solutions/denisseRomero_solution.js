307adca507326a3ad7cb1f2c4a1145e05aa9ef21
Denisse Romero <denisse.romero.c@gmail.com>
2026-01-26 21:45:30 -0500

    Solución Reto 07-Sumatoria

---
+++ challenges/07-sumatoria/solutions/denisseRomero_solution.js
@@ -0,0 +1,12 @@
+/**
+ * NOMBRE DEL RETO: [Pon aquí el nombre]
+ * SOLUCIONADO POR: [Tu Nombre/GitHub]
+ */
+function sumaNumeroEntero(A, B) {
+  let unionNumeros = A.toString() + B.toString();
+  return parseInt(unionNumeros);
+    
+}
+
+// Pruebas (esto sirve para verificar)
+console.log(sumaNumeroEntero(5, 3));
